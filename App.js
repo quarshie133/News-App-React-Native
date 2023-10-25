0ebd7e5ab3d3d188c6bf0b7045a4f7f4fd044ba7
quarshie133 <wrightsprince@gmail.com>
2023-10-25 11:57:26 +0000

    News Application

---
+++ App.js
@@ -0,0 +1,76 @@
+import { StyleSheet, Text, View } from "react-native";
+
+// Tab Navigator
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// Screens
+import All from "./screens/All";
+import Business from "./screens/Business";
+import HealthScreen from "./screens/Health";
+import SportsScreen from "./screens/Sports";
+import TechScreen from "./screens/Tech";
+import { Icon } from "react-native-elements";
+
+
+const Tab = createBottomTabNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="All"
+          component={All}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type="ionicon" name="home" color={props.color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Business"
+          component={Business}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type="feather" name="dollar-sign" color={props.color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Health"
+          component={HealthScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type="feather" name="heart" color={props.color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Sports"
+          component={SportsScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type="ionicon" name="tennisball-outline" color={props.color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Tech"
+          component={TechScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type="ionicon" name="hardware-chip-outline" color={props.color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = `a0ef234cb38145efa159b25dabf8bcc6`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const country = 'in'
